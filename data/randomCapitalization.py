ea44ed03255ba0f8e45215f07fd8f17fbd109de0
aaronzhao <aaron.zhaocr@gmail.com>
2021-06-09 21:08:48 +0200

    provide more training data to distinguish group chat with command

--- data/randomCapitalization.py
+++ data/randomCapitalization.py
@@ -1,11 +1,12 @@
 import re
 import random
 from datetime import datetime
-regex = re.compile('( *- )(@bot)(.*)')
-candidates = ['@bot', '@Bot', '@bOt', '@boT', '@bOT', '@BoT', '@BOt', '@BOT']
+regex = re.compile('- (.*)')
+# candidates = ['@bot', '@Bot', '@bOt', '@boT', '@bOT', '@BoT', '@BOt', '@BOT']
+candidates = ['@', '@b', '@ ', '@b ', '@B ', '@B', '@bo', '@bo ', '@bt', '@BT ', '@Bt', '@Bo ', '@bO', '@oT', '@ot ']
 random.seed(datetime.now())
 
-f = open('nlu.yml', 'r')
+f = open('text.txt', 'r')
 lines = f.readlines()
 f.close()
 
@@ -14,8 +15,8 @@ for line in lines:
     m = regex.match(line)
     s = ''
     if (m):
-        prefix = candidates[int(random.random() * 8)]
-        res += m.group(1) + prefix + m.group(3) + '\n'
+        prefix = candidates[int(random.random() * 15)]
+        res += '- ' + prefix + m.group(1) + '\n'
     else:
         res += line
         
