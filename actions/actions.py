bb94c3474446d59c2b53bd28a43fc3a279081325
aaronzhao <aaron.zhaocr@gmail.com>
2021-06-19 18:13:34 +0200

    query API for result

--- actions/actions.py
+++ actions/actions.py
@@ -13,28 +13,18 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import AllSlotsReset, FollowupAction
 
-import re
+import requests
+import json
 
-# class MyCustomAction(Action):
-    
-#     def name(self) -> Text:
-#         pass
-    
-#     def run(self, dispatcher:CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#             pass
-    
-#     def __parseContent(message):
-#         regex = re.compile('^@bot(.*)', re.IGNORECASE)
-#         match = regex.match(message)
-#         if not match:
-#             return False, message
-#         else:
-#             return True, match.group(1)
-        
-        
-    
+json_file= open('api.json')
+filterv2 = json.load(json_file)
+json_file.close()
+cuisineType = {}
+for option in filterv2['filters_v2']['filter_sections'][1]['filter_groups'][0]['options']:
+    cuisineType[option['label'].lower()] = option['value']
+dietaryRestrictions = {}
+for restriction in filterv2['filters_v2']['filter_sections'][2]['filter_groups'][0]['options']:
+    dietaryRestrictions[restriction['label'].lower()] = restriction['value']
 
 
 
@@ -44,42 +34,100 @@ class ActionConfirmFoodPreference(Action):
         return "action_confirm_food_preference"
 
     def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        
-        if tracker.slots['food']:
-            dispatcher.utter_message(
-                json_message={
-                    "text": "So, you are looking for a restaurant at {loc} and serves {food}" \
-                        .format(loc=tracker.slots['location'], food=tracker.slots['food']),
-                    "type": "confirm"
-                }
-            )
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    
+    
+        dispatcher.utter_message(
+            json_message={
+                "text": " Looking for a {cuisine} restaurant in {loc} that serves {restriction} food?" \
+                    .format(loc=tracker.slots['location'], cuisine=tracker.slots['cuisine'], restriction=tracker.slots['restriction']),
+                "type": "confirm"
+            }
+        )
 
         return []
 
 
 class ActionSendQuery(Action): 
-    
+    def __init__(self):
+        self.loc_dic = {}
+
     def name(self) -> Text:
         return "action_send_query"
+        
+    def __findCuisineId(self, tracker):
+        targetCuisine = tracker.slots['cuisine'].lower()
+        if targetCuisine in cuisineType:
+            return cuisineType[targetCuisine]
+        else:
+            return 'undefined'
+
+
+    def __findRestrictionId(self, tracker):
+        targetRestriction = tracker.slots['restriction'].lower()
+        if targetRestriction in dietaryRestrictions:
+            return dietaryRestrictions[targetRestriction]
+        else:
+            return 'undefined'
+
 
     def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        
-        if tracker.slots['food']:
-            dispatcher.utter_message(
-                json_message={
-                    "text": " \"{food}\" AND \"{loc}\"" \
-                        .format(loc=tracker.slots['location'], food=tracker.slots['food']),
-                    "type": "result"
-                }
-            )
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        loc = tracker.slots['location']
+
+        headers = {
+            'x-rapidapi-key': "0f938c1e2bmsh3328e331fae389cp1830bajsn6682184f42a9",
+            'x-rapidapi-host': "travel-advisor.p.rapidapi.com"
+        }
+
+        loc_id = 188626
+
+        if loc in self.loc_dic:
+            loc_id = self.loc_dic[loc]
+        else:
+            url = "https://travel-advisor.p.rapidapi.com/locations/search"
+            querystring = {"query":"{location}".format(location=tracker.slots['location']),"limit":"30","offset":"0",\
+                "units":"km","location_id":"1","currency":"USD","sort":"relevance","lang":"en_US"}
+
+            response = requests.request("GET", url, headers=headers, params=querystring)
+
+            dict_response = response.json()        
+            loc_id = dict_response['data'][0]['result_object']['location_id']
+            self.loc_dic[loc] = loc_id
+
+        url = "https://travel-advisor.p.rapidapi.com/restaurants/list"
+        querystring = {"location_id":"{location}".format(location=loc_id),"restaurant_tagcategory":"10591","restaurant_tagcategory_standalone":"10591",\
+                "combined_food":"{cuisine}".format(cuisine = self.__findCuisineId(tracker)),"currency":"USD","lunit":"km",\
+                "dietary_restrictions":"{restriction}".format(restriction= self.__findRestrictionId(tracker)),"limit":"30","open_now":"false","lang":"en_US"}
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        res_json = response.json()
+
+        result = {
+            "text": [],
+            "type": "result"
+        } 
+        for rest in res_json['data']:
+            if  'name' in rest:
+                result['text'].append({
+                    "name": rest['name'],
+                    "image": rest['photo']['images']['thumbnail'] if 'photo' in rest else "",
+                    "rating": rest['rating'],
+                    "price_level": rest['price_level'] if 'price_level' in rest else "",
+                    "website": rest['website'] if "website" in rest else "",
+                    "address": rest['address'],
+                    "cuisine": rest['cuisine'] if "cuisine" in rest else ""
+                })
+
+        dispatcher.utter_message(
+            json_message= result
+        )
 
         return [AllSlotsReset()]
+
     
 
 class ActionMonitorGroupChat(Action):
@@ -90,7 +138,18 @@ class ActionMonitorGroupChat(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
             
-            if not tracker.slots['food'] and not tracker.slots['location']:
+            if not tracker.slots['cuisine'] and not tracker.slots['location'] and not tracker.slots['restriction']:
                 return [FollowupAction(name= 'utter_ignore')] 
             else:
                 return []
+
+class ActionResetPreferences(Action):
+    def name(self) -> Text:
+        return "action_reset_preferences"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            return [AllSlotsReset()]
+
